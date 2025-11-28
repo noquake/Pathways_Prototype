d4d2b280649f79d15aafb1fe9fe7d0e420caeadd
kwaku.aa <kwakuagyapong478@gmail.com>
2025-11-28 18:24:46 -0500

    last commits to merge

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# from scrap_docs import main as scrape_docs
+from scrape_docs import main as scrape_docs
 from transform_data import main as transform_data
 from chunk_and_embed import main as chunk_and_embed
 
@@ -7,13 +7,13 @@ from chunk_and_embed import main as chunk_and_embed
 
 def main():
     # retrieve the needed clinical documentation
-    # scrape_docs()
+    scrape_docs()
 
     # transform the retrieved documents into usable data formats
     transform_data()
 
     # chunk and embed the transformed data for use in retrieval-augmented generation
     chunk_and_embed()
-    
+
 if __name__ == "__main__": 
     main()
