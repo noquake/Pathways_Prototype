4b3eab380abae509529c5c56ad0e4c193b9509c1
W-Geedorah <kwakuagyapong478@gmail.com>
2026-01-27 12:30:14 -0500

    still fixing backend service

--- services/backend/app.py
+++ services/backend/app.py
@@ -17,19 +17,19 @@ def main():
     from sentence_transformers import SentenceTransformer
     """ spin up all docker containers ->  prepare data for RAG -> chunk and embed data into pathways_db """
 
-    # create and start all docker containers
-    print("Spinning up docker containers...\n")
-    spin_up_docker()
-    print("Docker containers are up and running.\n")
+    # create and start all docker containers ONLY UNCOMMENT WHEN TESTING LOCALLY
+    # print("Spinning up docker containers...\n")
+    # spin_up_docker()
+    # print("Docker containers are up and running.\n")
 
     # scrape documents from specified sources
     print("Starting document scraping...\n")
-    scrape_docs()
+    # scrape_docs()
     print("Document scraping complete.\n")
     
     # transform the retrieved documents into usable data formats
     print("Transforming data for RAG...\n")
-    transform_data()
+    # transform_data()
     print("Data transformation complete.\n")
 
     # chunk and embed the transformed data for use in retrieval-augmented generation
