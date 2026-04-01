f93fdbebb77603d3c76d4b3be5b0e5d69a3a1edd
kwaku.aa <kwakuagyapong478@gmail.com>
2026-04-01 19:47:10 -0400

    addig line for main.py deployment on vps

--- services/backend/main.py
+++ services/backend/main.py
@@ -56,6 +56,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://frontend:3000",
+        "https://72.60.170.111:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
